ea375ada385a621edd661d19f3305f0eadc7cd07
El Saico <bruno@elsai.co>
2011-04-18 20:26:09 +0000

    Ajustando código do servidor

--- hdlc_server.c
+++ hdlc_server.c
@@ -1,46 +1,47 @@
-/* Creates a datagram server.  The port 
-   number is passed as an argument.  This
-   server runs forever */
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define CONN_PORT 64000
+#include "hdlc.h"
 
-void error(char *msg)
-{
-    perror(msg);
-    exit(0);
+void error(char *msg) {
+	perror(msg);
+	exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-   int sock, length, fromlen, n;
-   struct sockaddr_in server;
-   struct sockaddr_in from;
-   char buf[1024];
-
-   sock=socket(AF_INET, SOCK_DGRAM, 0);
-   if (sock < 0) error("Opening socket");
-   length = sizeof(server);
-   bzero(&server,length);
-   server.sin_family=AF_INET;
-   server.sin_addr.s_addr=INADDR_ANY;
-   server.sin_port=htons(CONN_PORT);
-   if (bind(sock,(struct sockaddr *)&server,length)<0) 
-       error("binding");
-   fromlen = sizeof(struct sockaddr_in);
-   while (1) {
-       n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
-       if (n < 0) error("recvfrom");
-       write(1,"Received a datagram: ",21);
-       write(1,buf,n);
-       n = sendto(sock,"Got your message\n",17,
-                  0,(struct sockaddr *)&from,fromlen);
-       if (n  < 0) error("sendto");
-   }
- }
-
+int main(int argc, char *argv[]) {
+	if ((argc != 3) || (strcmp(argv[1], "-h") != 0)) {
+		printf("Usage: hdlc_server -h ip\n");
+		exit(1);
+	}
+	
+	int len_send, len_recv;
+	struct sockaddr_in server;
+	uint8_t buf_send[256], buf_recv[256];
+	
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) error("Opening socket");
+	
+	struct hostent* hp = gethostbyname(argv[2]);
+	if (hp == 0) error("Unknown host");
+	
+	memcpy(&server.sin_addr, hp->h_addr, hp->h_length);	
+	server.sin_family = AF_INET;
+	server.sin_port = htons(CONN_PORT);
+	socklen_t length = sizeof(server);
+	
+	if (bind(sock, (struct sockaddr *)&server, length) < 0) 
+		error("binding");
+	
+	while (1) {
+		//len_recv = recvfrom(sock,buf,1024,0,(struct sockaddr *)&server,&length);
+		//len_send = sendto(sock,"Got your message\n",17,0,(struct sockaddr *)&server,length);
+	}
+	
+	return 0;
+}
